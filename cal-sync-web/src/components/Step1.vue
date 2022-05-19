dd7ee6e173b7c58969ddae38e4c1f859a40d2a86
Nick Patterson <nkpatterson@github.com>
2022-05-19 19:39:11 +0000

    Stubbed out API and wizard flow

---
+++ cal-sync-web/src/components/Step1.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { Api } from '../services/api-service'
+import ProgressBar from './ProgressBar.vue'
+import Instructions from './Instructions.vue'
+import { defineComponent } from 'vue'
+</script>
+
+<script lang="ts">
+export default defineComponent({
+  // type inference enabled
+  props: {
+    name: String,
+    msg: { type: String, required: true }
+  },
+  data() {
+    return {
+      count: 1
+    }
+  },
+  mounted() {
+    this.name // type: string | undefined
+    this.msg // type: string
+    this.count // type: number
+  }
+})
+</script>
+<template>
+    <ProgressBar />
+    <Instructions />
+    <h1>Step {{ count }}</h1>
+    <p>This is the first step towards enlightenment.</p>
+    <router-link to="/google">Continue</router-link>
+</template>
---
+++ cal-sync-web/src/services/api-service.ts
@@ -0,0 +1,48 @@
+enum CalendarApi {
+    GoogleCalendar,
+    Office365Calendar
+}
+
+class ExternalConnection {
+    public ResourceId: string = "";
+    public ConsentLink: string = "";
+
+    constructor(resourceId: string, consentLink: string) {
+        this.ResourceId = resourceId;
+        this.ConsentLink = consentLink;
+    }
+}
+
+class Api {
+    apiUrlBase: string = "https://nkp-calsync-funcs.azurewebsites.net/api/";
+
+    public getConsentCodeFromUrl(url: string): string {
+        let regex = new RegExp('(code=)(.*)$');
+        let result = regex.exec(url);
+        if (result != null && result.length > 0) {
+            return result[1];
+        }
+
+        return "";
+    }
+
+    public createResourceGroup(username: string): string {
+        // returns newly created Resource Group Name
+        return "";
+    }
+
+    public createExternalConnection(api: CalendarApi, username: string, resourceGroupName: string): ExternalConnection {
+        // TODO: 
+        return new ExternalConnection("blah", "https://godosomething.com");
+    }
+
+    public confirmConsentCode(consentCode: string, resourceId: string): boolean {
+        return true;
+    }
+
+    public deployLogicApps(resourceGroupName: string, googleResourceId: string, googleCalendarId: string, office365ResourceId: string): boolean {
+        return true;
+    }
+}
+
+export { Api, CalendarApi, ExternalConnection }
